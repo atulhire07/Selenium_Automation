865bb6a39e4084a65bcf75fc5b74c49016664d7b
Atul <atulhire55@gmail.com>
2025-11-14 11:42:23 +0530

    new file added

---
+++ src/day4/IfConditionDemo.java
@@ -0,0 +1,17 @@
+package day4;
+
+public class IfConditionDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int person_age=15;
+		
+		if(person_age>=18)
+		 {
+			 System.out.println("eligible for vote");
+		 }
+		
+	}
+
+}
